3e22e48494aa8d4a24549957090ce14e08d1a3c6
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-09-01 12:14:58 +0000

    Completed confirmations emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -75,7 +75,7 @@ class StripeWH_Handler:
             profile.default_town_or_city=shipping_details.address.city
             profile.default_street_address1=shipping_details.address.line1
             profile.default_street_address2=shipping_details.address.line2
-            profile.default_county = shipping_detaess.state
+            profile.default_county = shipping_details.address.state
             profile.save()
 
         order_exists = False
